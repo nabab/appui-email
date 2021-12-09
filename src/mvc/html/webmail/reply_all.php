5c82ce391fd2ed02e3c3f07d2d5bdabb519ff658
qr.dev.bbn.io <qr@qr.dev.m3l.co>
2021-12-09 16:23:57 +0100

    add reply, reply all, forward, open in new tab and delete

--- src/mvc/html/webmail/reply_all.php
+++ src/mvc/html/webmail/reply_all.php
@@ -1,30 +1,48 @@
-<div class="bbn-overlay bbn-flex-height">
-  <div class="bbn-header">
+<div class="bbn-overlay bbn-flex-height email-header">
+  <div class="bbn-header mail-editor-header">
     <bbn-toolbar class="bbn-m">
-      <bbn-button></bbn-button>
-      <bbn-switch v-model="switchValue"
-                  value="markdown"
-                  novalue="rte"></bbn-switch>
+      <div class="bbn-flex-width">
+            <bbn-button :text="trlt.editor"
+                :disabled="true"
+                class="subjectButton"
+                style=" margin-right: 1%; border:none;"></bbn-button>
+        <bbn-dropdown v-model="type"
+                      :source="editors"></bbn-dropdown>
+      </div>
+      <div class="bbn-flex-width">
+      </div>
+      <div class="bbn-flex">
+        <bbn-button text="CC"
+                    @click="ccChange"
+                    :style="{color : ccButton ? 'green' : 'red'}"></bbn-button>
+        <bbn-button text="CCI"
+                    @click="cciChange"
+                    :style="{color : cciButton ? 'green' : 'red'}"></bbn-button>
+        <bbn-button :text="trlt.send"
+                    icon="nf nf-fa-send"
+                    style="color: blue"></bbn-button>
+      </div>
     </bbn-toolbar>
   </div>
-  <div class="bbn-flex-fill bbn-grid-fields">
-		<bbn-initial
-                 :letters="trlt.to"></bbn-initial>
+  <div class="bbn-flex-fill mail-grid-fields email-header">
+    <bbn-button :text="trlt.to"></bbn-button>
     <bbn-input v-model="replyTo"></bbn-input>
-    <bbn-initial
-                 letters="CC"></bbn-initial>
-    <bbn-input v-model="CC"></bbn-input>
-    <bbn-initial
-                 letters="CCI"></bbn-initial>
-    <bbn-input v-model="CCI"></bbn-input>
-    <bbn-initial
-                 :letters="trlt.subject"></bbn-initial>
+    <bbn-button text="CC"
+                v-if="ccButton"></bbn-button>
+    <bbn-input v-model="CC"
+               v-if="ccButton"></bbn-input>
+    <bbn-button text="CCI"
+                v-if="cciButton"></bbn-button>
+    <bbn-input v-model="CCI"
+               v-if="cciButton"></bbn-input>
+    <bbn-button :text="trlt.subject"
+                :disabled="true"
+                class="subjectButton"
+                style="border:none;"></bbn-button>
     <bbn-input v-model="subject"></bbn-input>
   </div>
   <div class="bbn-flex-fill">
-    <bbn-markdown v-model="emailHeader + source.html"
-                  v-if=" switchValue === 'markdown'"></bbn-markdown>
-    <bbn-rte v-model="emailHeader + source.html"
-             v-if=" switchValue === 'rte' "></bbn-rte>
+        <component :is="'bbn-' + type"
+                    v-model="source.html"></component>
   </div>
 </div>
