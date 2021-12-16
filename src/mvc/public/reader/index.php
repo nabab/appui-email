7aee2bde72438ae2e833029407c7a95df0a55baa
qr.dev.bbn.io <qr@qr.dev.m3l.co>
2021-12-16 14:39:24 +0100

    commit

--- src/mvc/public/reader/index.php
+++ src/mvc/public/reader/index.php
@@ -5,8 +5,10 @@
  * @var $ctrl \bbn\Mvc\Controller 
  *
  */
+use bbn\X;
 if ($ctrl->hasArguments()) {
   if ($model = $ctrl->getModel(['id' => $ctrl->arguments[0]])) {
+    X::ddump($model);
     $doc = new DOMDocument();
     $dom = $doc->loadHTML($model['html']);
     //die(var_dump($doc));
