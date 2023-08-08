825fcdd76a52c0e4c331dacae24802434e6c573c
nabab <thomas.nabet@gmail.com>
2023-08-08 10:01:18 +0200

    Indentation

--- src/cli/process.php
+++ src/cli/process.php
@@ -1,5 +1,9 @@
 <?php
-$mailing = new \bbn\Appui\Mailing($ctrl->db);
+
+use bbn\Appui\Mailing;
+
+$mailing = new Mailing($ctrl->db);
+
 if ( ($result = $mailing->process(30)) && isset($result['sent'], $result['successes']) ){
   echo _("Processed").': '.$result['sent'].PHP_EOL._("Sent").': '.$result['successes'];
 }
