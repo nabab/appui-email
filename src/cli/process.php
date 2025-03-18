a8121dd18f4b86a5980978616ae317d5f89b6de7
nabab <thomas.nabet@gmail.com>
2025-03-18 12:15:11 +0100

    Latest

--- src/cli/process.php
+++ src/cli/process.php
@@ -4,6 +4,6 @@ use bbn\Appui\Mailing;
 
 $mailing = new Mailing($ctrl->db);
 
-if ( ($result = $mailing->process(30)) && isset($result['sent'], $result['successes']) ){
-  echo _("Processed").': '.$result['sent'].PHP_EOL._("Sent").': '.$result['successes'];
+if ($result = $mailing->process(30)) {
+  echo _(sprintf("Processed %d mail", $result));
 }
