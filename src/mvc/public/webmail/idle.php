8033de08e27b12b71c99f2c3cbf27b55613096d8
Mirko Argentino <mirko@bbn.solutions>
2026-02-08 08:55:13 +0100

    imap idle

--- src/mvc/public/webmail/idle.php
+++ src/mvc/public/webmail/idle.php
@@ -2,49 +2,32 @@
 use bbn\User\Email;
 /** @var $ctrl \bbn\Mvc\Controller */
 
-if (!empty($ctrl->post['start'])) {
-  if (($email = new Email($ctrl->db))
-    && ($mailbox = $email->getMailbox($ctrl->post['start']))
-    && !$mailbox->getIdleStream()
-  ) {
-    $mailbox->idle();
+$ctrl->setStream();
+if (!empty($ctrl->post['account'])
+  && ($email = new Email($ctrl->db))
+  && ($mailbox = $email->getMailbox($ctrl->post['account']))
+) {
+  try {
+    $idle = $mailbox->idle(function($message) use ($ctrl) {
+      $ctrl->stream([
+        'message' => $message
+      ]);
+    }, null, $ctrl);
   }
-}
-elseif (!empty($ctrl->post['stop'])) {
-  if (($email = new Email($ctrl->db))
-    && ($mailbox = $email->getMailbox($ctrl->post['stop']))
-    && $mailbox->getIdleStream()
-  ) {
+  catch (Exception $e) {
+    bbn\X::log($e->getMessage(), 'mirko_idle');
+    $ctrl->stream([
+      'success' => false,
+      'error' => $e->getMessage(),
+      'errorCode' => $e->getCode()
+    ]);
+  }
+
+  if ($mailbox->getIdleStream()) {
     $mailbox->stopIdle();
   }
 }
-else {
-  $ctrl->setStream();
-  if (!empty($ctrl->post['account'])
-    && ($email = new Email($ctrl->db))
-    && ($mailbox = $email->getMailbox($ctrl->post['account']))
-  ) {
-    try {
-      $idle = $mailbox->idle(function($message) use ($ctrl) {
-        $ctrl->stream([
-          'message' => $message
-        ]);
-      });
-    }
-    catch (Exception $e) {
-      $ctrl->stream([
-        'success' => false,
-        'error' => $e->getMessage(),
-        'errorCode' => $e->getCode()
-      ]);
-    }
 
-    if ($mailbox->getIdleStream()) {
-      $mailbox->stopIdle();
-    }
-  }
-
-  $ctrl->stream([
-    'success' => true
-  ]);
-}
+$ctrl->stream([
+  'success' => true
+]);
