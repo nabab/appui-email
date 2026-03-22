ec4d1dea59c30cd73d94df4e0e5cccc60c90dee5
nabab <thomas.nabet@gmail.com>
2026-03-23 00:37:26 +0100

    New mvc-attached constant system

--- src/mvc/public/mailing.php
+++ src/mvc/public/mailing.php
@@ -4,7 +4,7 @@ use bbn\X;
 /** @var bbn\Mvc\Controller $ctrl */
 $ctrl->setColor('teal', 'white');
 $root = $ctrl->pluginUrl('appui-email') . '/';
-if (!defined('BBN_BASEURL') || empty(constant('BBN_BASEURL'))) {
+if (!$ctrl->getConstant('baseURL')) {
   $ctrl->setUrl($root . 'mailing')
     ->setIcon('nf nf-fa-envelope')
     ->combo(_("eMails"), ['root' => $root, 'page' => $ctrl->arguments[0] ?? '']);
